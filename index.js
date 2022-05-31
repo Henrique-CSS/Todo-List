bdaf369b817c5e23f5e3901378de1e3328912cb0
Henrique.CSS <97364623+Henrique-CSS@users.noreply.github.com>
2022-05-31 11:26:09 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+
+listItens = [];
+
+function getListItens() {
+    num = listItens.length;
+    if (document.getElementById('todo_input').value == '') {
+        alert('Write an iten to put in the list');
+    } else {
+        listItens[num] = document.getElementById('todo_input').value;
+    }
+}
+
+function addItem() {
+
+    getListItens();
+    list = document.getElementById('List-todo');
+    list.innerHTML = '';
+    document.getElementById('todo_input').value = '';
+    var ItemCount = 1;
+    for (var i = 0; i < listItens.length; i++) {
+        
+        if (listItens[i] != '') {
+            list.innerHTML += '<span id="Item' + i + '"><input type="checkbox"><span class="itens">' + '    Item ' + ItemCount + " - " + listItens[i] + '</span><input type="button" value="X" onclick="deleteItem(' + i + ')"></span><br>';
+        } else {
+            list.innerHTML += '';
+            ItemCount = ItemCount - 1;
+        }
+        ItemCount++;
+    }
+}
+
+function deleteItem(value) {
+    listItens[value] = '';
+    document.getElementById('Item' + value).remove;
+    addItem();
+}
